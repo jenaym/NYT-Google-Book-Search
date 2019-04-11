2c22430360065edb709e5a2a3f1d1b537b7632db
JAngel <43564263+jenaym@users.noreply.github.com>
2019-04-10 17:06:14 -0700

    added deployment info

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const path = require("path")
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
 
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // Set up the main page per environment
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
